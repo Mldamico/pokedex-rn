b3e9919fa83c470a15a73424fa2483ef7ef860fc
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2021-04-23 11:27:35 -0300

    Initial setup

---
+++ src/navigation/StackNavigation.tsx
@@ -0,0 +1,14 @@
+import {createStackNavigator} from '@react-navigation/stack';
+import React from 'react';
+import {HomeScreen} from '../screens/HomeScreen';
+import {PokemonScreen} from '../screens/PokemonScreen';
+const Stack = createStackNavigator();
+
+export function StackNavigation() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="PokemonScreen" component={PokemonScreen} />
+    </Stack.Navigator>
+  );
+}
---
+++ src/screens/PokemonScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export const PokemonScreen = () => {
+  return (
+    <View>
+      <Text>Pok</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
