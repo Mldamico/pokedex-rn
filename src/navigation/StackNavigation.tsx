4bdd76850c87086cc8c7eb2906ba734669e9624f
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2021-04-23 11:32:01 -0300

    Icons and stack navigator

--- src/navigation/StackNavigation.tsx
+++ src/navigation/StackNavigation.tsx
@@ -2,6 +2,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
 import {HomeScreen} from '../screens/HomeScreen';
 import {PokemonScreen} from '../screens/PokemonScreen';
+
 const Stack = createStackNavigator();
 
 export function StackNavigation() {
