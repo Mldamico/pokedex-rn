33d838d8bb1297cbd5dffbc7c1894f00e6d3eff8
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2021-04-24 10:30:33 -0300

    SearchScreen layout

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export const SearchInput = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.textBackground}>
+        <TextInput
+          placeholder="Search Pokemon"
+          style={styles.textInput}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholderTextColor="#aaa"
+        />
+        <Icon name="search-outline" color="grey" size={30} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+  textBackground: {
+    backgroundColor: '#f3f1f3',
+    borderRadius: 50,
+    height: 40,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+
+    elevation: 10,
+  },
+  textInput: {
+    flex: 1,
+    fontSize: 18,
+    color: '#555',
+  },
+});
