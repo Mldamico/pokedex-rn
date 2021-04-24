4d56cbd8495766d3c401ba2df8ff0b415081c4bd
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2021-04-24 11:32:37 -0300

    Find pokemon by id

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -25,11 +25,17 @@ export const SearchScreen = () => {
     if (term.length === 0) {
       return setPokemonFiltered([]);
     }
-    setPokemonFiltered(
-      simplePokemonList.filter(poke =>
-        poke.name.toLowerCase().startsWith(term.toLowerCase()),
-      ),
-    );
+
+    if (isNaN(Number(term))) {
+      setPokemonFiltered(
+        simplePokemonList.filter(poke =>
+          poke.name.toLowerCase().startsWith(term.toLowerCase()),
+        ),
+      );
+    } else {
+      const pokemonById = simplePokemonList.find(poke => poke.id === term);
+      setPokemonFiltered(pokemonById ? [pokemonById] : []);
+    }
   }, [term]);
 
   if (isFetching) {
